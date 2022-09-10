226b420a92a7c28cac78d9f102ee78575ba61c8a
msyafiqq <id.muhammad.syafiq@gmail.com>
2022-09-10 21:47:48 +0700

    reset stun

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -37,13 +37,7 @@
 
     const createPeerConnection = () => {
         const pc = new RTCPeerConnection({
-            iceServers: [
-                { urls: "stun:stun.l.google.com:19302", },
-                { urls: "stun:stun1.l.google.com:19302", },
-                { urls: "stun:stun2.l.google.com:19302", },
-                { urls: "stun:stun3.l.google.com:19302", },
-                { urls: "stun:stun4.l.google.com:19302" }
-            ],
+            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
         });
 
         pc.onnegotiationneeded = async () => {
